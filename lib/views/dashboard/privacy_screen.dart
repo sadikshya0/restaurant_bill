173e2d8ac5276af1f7daee0101a3120b810a2a74
sadikshya0 <sadikshyapdl02@gmail.com>
2026-05-08 13:13:24 +0545

    profile_screen_updated

---
+++ lib/views/dashboard/privacy_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:restaurant_bill/utils/colors.dart';
+import 'package:restaurant_bill/utils/custom_text_styles.dart';
+import 'package:restaurant_bill/views/dashboard/profile_screen.dart';
+
+class PrivacyScreen extends StatelessWidget {
+  const PrivacyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () {
+            Get.offAll(() => ProfileScreen());
+          },
+          child: Icon(Icons.arrow_back, color: AppColors.textColor, size: 25),
+        ),
+        title: Center(
+          child: Text(
+            "Privacy and security",
+            style: CustomTextStyles.f18W600(color: AppColors.textColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/dashboard/profile_screen.dart
+++ lib/views/dashboard/profile_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:restaurant_bill/controller/dashboard/profile_controller.dart';
 import 'package:restaurant_bill/utils/colors.dart';
 import 'package:restaurant_bill/utils/custom_text_styles.dart';
 import 'package:restaurant_bill/utils/image_path.dart';
 import 'package:restaurant_bill/views/auth/login_screen.dart';
+import 'package:restaurant_bill/views/auth/register_screen.dart';
+import 'package:restaurant_bill/views/dashboard/about_screen.dart';
+import 'package:restaurant_bill/views/dashboard/edit_profile_screen.dart';
+import 'package:restaurant_bill/views/dashboard/favourite_screen.dart';
+import 'package:restaurant_bill/views/dashboard/help_screen.dart';
+import 'package:restaurant_bill/views/dashboard/home_screen.dart';
+import 'package:restaurant_bill/views/dashboard/privacy_screen.dart';
 import 'package:restaurant_bill/widgets/profile_widgets/profile_menu_tile.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  final ProfileController controller = Get.put(ProfileController());
+  ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +24,12 @@ class ProfileScreen extends StatelessWidget {
       backgroundColor: AppColors.whiteColor,
 
       appBar: AppBar(
+        leading: InkWell(
+          onTap: () {
+            Get.offAll(() => HomeScreen());
+          },
+          child: Icon(Icons.arrow_back, color: AppColors.textColor, size: 25),
+        ),
         title: Center(
           child: Text(
             "My Profile",
@@ -27,14 +42,11 @@ class ProfileScreen extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(16),
-
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-
               children: [
                 Container(
                   padding: const EdgeInsets.only(top: 16, bottom: 18),
-
                   width: double.infinity,
 
                   decoration: BoxDecoration(
@@ -48,58 +60,98 @@ class ProfileScreen extends StatelessWidget {
                     children: [
                       Stack(
                         children: [
-                          Container(
-                            height: 95,
-                            width: 95,
-
-                            decoration: BoxDecoration(
-                              color: AppColors.lGrey,
-                              shape: BoxShape.circle,
-
-                              border: Border.all(
-                                color: AppColors.primaryColor.withOpacity(0.75),
-                                width: 4,
+                          Obx(
+                            () => Container(
+                              height: 95,
+                              width: 95,
+                              decoration: BoxDecoration(
+                                color: AppColors.lGrey,
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: AppColors.primaryColor.withOpacity(
+                                    0.75,
+                                  ),
+                                  width: 4,
+                                ),
                               ),
-                            ),
-
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-
-                              child: Image.asset(
-                                ImagePath.profile,
-                                height: 60,
-                                width: 60,
+                              child: ClipOval(
+                                child: controller.selectedImage.value != null
+                                    ? Image.file(
+                                        controller.selectedImage.value!,
+                                        fit: BoxFit.cover,
+                                      )
+                                    : Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Image.asset(
+                                          ImagePath.profile,
+                                          height: 60,
+                                          width: 60,
+                                        ),
+                                      ),
                               ),
                             ),
                           ),
-
                           Positioned(
                             bottom: 4,
                             right: 8,
-
-                            child: Container(
-                              height: 25,
-                              width: 25,
-
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: AppColors.whiteColor,
-
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: AppColors.backGroundDark.withOpacity(
-                                      0.15,
+                            child: GestureDetector(
+                              onTap: () {
+                                Get.bottomSheet(
+                                  Container(
+                                    padding: const EdgeInsets.all(20),
+                                    decoration: BoxDecoration(
+                                      color: AppColors.whiteColor,
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(20),
+                                        topRight: Radius.circular(20),
+                                      ),
                                     ),
 
-                                    blurRadius: 2,
-                                    spreadRadius: 1.5,
-                                    offset: const Offset(0, 3),
-                                  ),
-                                ],
-                              ),
+                                    child: Wrap(
+                                      children: [
+                                        ListTile(
+                                          leading: const Icon(Icons.camera_alt),
+                                          title: const Text("Camera"),
+                                          onTap: () async {
+                                            Get.back();
+                                            await controller.pickFromCamera();
+                                          },
+                                        ),
 
-                              child: const Center(
-                                child: Icon(Icons.camera_alt, size: 16),
+                                        ListTile(
+                                          leading: const Icon(Icons.photo),
+                                          title: const Text("Gallery"),
+                                          onTap: () async {
+                                            Get.back();
+                                            await controller.pickFromGallery();
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              },
+
+                              child: Container(
+                                height: 25,
+                                width: 25,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: AppColors.whiteColor,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: AppColors.backGroundDark
+                                          .withOpacity(0.15),
+                                      blurRadius: 2,
+                                      spreadRadius: 1.5,
+                                      offset: const Offset(0, 3),
+                                    ),
+                                  ],
+                                ),
+
+                                child: const Center(
+                                  child: Icon(Icons.camera_alt, size: 14),
+                                ),
                               ),
                             ),
                           ),
@@ -126,20 +178,25 @@ class ProfileScreen extends StatelessWidget {
 
                       const SizedBox(height: 10),
 
-                      Container(
-                        height: 40,
-                        width: 100,
+                      InkWell(
+                        onTap: () {
+                          Get.offAll(() => EditProfileScreen());
+                        },
+                        child: Container(
+                          height: 40,
+                          width: 100,
 
-                        decoration: BoxDecoration(
-                          color: AppColors.textColor.withOpacity(0.9),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
+                          decoration: BoxDecoration(
+                            color: AppColors.textColor.withOpacity(0.9),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
 
-                        child: Center(
-                          child: Text(
-                            "Edit Profile",
-                            style: CustomTextStyles.f12W600(
-                              color: AppColors.whiteColor,
+                          child: Center(
+                            child: Text(
+                              "Edit Profile",
+                              style: CustomTextStyles.f12W600(
+                                color: AppColors.whiteColor,
+                              ),
                             ),
                           ),
                         ),
@@ -159,25 +216,51 @@ class ProfileScreen extends StatelessWidget {
 
                 const SizedBox(height: 10),
 
-                ProfileMenuTile(title: "Favourites", iconPath: ImagePath.heart),
-
-                const SizedBox(height: 10),
-
-                ProfileMenuTile(
-                  title: "Privacy and security",
-                  iconPath: ImagePath.privacy,
+                InkWell(
+                  onTap: () {
+                    Get.offAll(() => FavouriteScreen());
+                  },
+                  child: ProfileMenuTile(
+                    title: "Favourites",
+                    iconPath: ImagePath.heart,
+                  ),
                 ),
 
                 const SizedBox(height: 10),
 
-                ProfileMenuTile(
-                  title: "Help and support",
-                  iconPath: ImagePath.help,
+                InkWell(
+                  onTap: () {
+                    Get.offAll(() => PrivacyScreen());
+                  },
+                  child: ProfileMenuTile(
+                    title: "Privacy and security",
+                    iconPath: ImagePath.privacy,
+                  ),
                 ),
 
                 const SizedBox(height: 10),
 
-                ProfileMenuTile(title: "About", iconPath: ImagePath.about),
+                InkWell(
+                  onTap: () {
+                    Get.offAll(() => HelpScreen());
+                  },
+                  child: ProfileMenuTile(
+                    title: "Help and support",
+                    iconPath: ImagePath.help,
+                  ),
+                ),
+
+                const SizedBox(height: 10),
+
+                InkWell(
+                  onTap: () {
+                    Get.offAll(() => AboutScreen());
+                  },
+                  child: ProfileMenuTile(
+                    title: "About",
+                    iconPath: ImagePath.about,
+                  ),
+                ),
 
                 const SizedBox(height: 40),
 
